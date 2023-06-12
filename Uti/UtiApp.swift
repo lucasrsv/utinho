4644b3e953935a416f0d0177ef9bf4edc7346b89
Lucas <lucasrsvalenca@gmail.com>
2023-06-12 10:26:34 -0300

    create xcode project

---
+++ Uti/UtiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtiApp.swift
+//  Uti
+//
+//  Created by lrsv on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UtiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
