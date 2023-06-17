7ffe76e358f37fac08638d6546676bb619d10e81
Lucas <lucasrsvalenca@gmail.com>
2023-06-17 20:21:23 -0300

    update uti store load method

--- Uti/Models/UtiStore.swift
+++ Uti/Models/UtiStore.swift
@@ -9,8 +9,7 @@ import Foundation
 import SwiftUI
 
 class UtiStore: ObservableObject {
-    // TODO: Define the default uti
-    @Published var uti: Uti = Uti(currentCycleDay: 1, phase: .folicular, state: .normal, leisure: 100, health: 100, nutrition: 100, blood: 100, items: [])
+    @Published var uti: Uti = Uti(currentCycleDay: 1, phase: .menstrual, state: .homely, leisure: 100, health: 100, nutrition: 100, blood: 100, items: [])
     
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
@@ -20,19 +19,13 @@ class UtiStore: ObservableObject {
     func load() async throws {
         let task = Task<Uti, Error> {
             let fileURL = try Self.fileURL()
-            guard let data = try? Data(contentsOf: fileURL) else {
-                return uti
-            }
+            let data = try Data(contentsOf: fileURL)
             let uti = try JSONDecoder().decode(Uti.self, from: data)
             return uti
         }
-        do {
-            let uti = try await task.value
-            DispatchQueue.main.async {
-                self.uti = uti
-            }
-        } catch {
-            fatalError(error.localizedDescription)
+        let uti = try await task.value
+        DispatchQueue.main.async {
+            self.uti = uti
         }
     }
     
@@ -44,8 +37,4 @@ class UtiStore: ObservableObject {
         }
         _ = try await task.value
     }
-}
-
-enum PersistenceError: Error {
-    case dataUnavailable
 }
