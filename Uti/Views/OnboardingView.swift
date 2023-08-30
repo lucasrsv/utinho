b079ebfe40a1b0cc2a2c352e17aa65300035bbaa
Michelly <silva.michelly@gmail.com>
2023-08-30 15:13:58 -0300

    first commit

---
+++ Uti/Views/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Uti
+//
+//  Created by michellyes on 30/08/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Onboarding aqui!")
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
